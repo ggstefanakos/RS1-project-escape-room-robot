03d9a61dd3bcc807b9a195a4432de736d94b6042
Zeroed <cocacc3m@gmail.com>
2026-06-08 13:50:36 +0300

    added delay to script

--- escape_room_ws/robot_start.sh
+++ escape_room_ws/robot_start.sh
@@ -17,7 +17,7 @@ gnome-terminal --tab --title="1.1 Odometry (ROS1)" -- bash -c "
 echo 'Φόρτωση ROS1...';
 source /opt/ros/noetic/setup.bash;
 roscore &
-sleep 5;
+sleep 15;
 echo 'Εκκίνηση Odom...';
 source /home/er/Desktop/Projects/myagv_ros/devel/setup.bash
 roslaunch myagv_odometry myagv_active.launch
@@ -26,7 +26,7 @@ exec bash"
 # Tab 2: Η Γέφυρα (ros1_bridge)
 gnome-terminal --tab --title="2. Bridge (ROS1+ROS2)" -- bash -c "
 echo 'Περιμένω το Lidar να ξεκινήσει...';
-sleep 8;
+sleep 20;
 echo 'Φόρτωση ROS1 & ROS2...';
 source /opt/ros/noetic/setup.bash;
 source /opt/ros/galactic/setup.bash;
@@ -38,7 +38,7 @@ exec bash"
 # Tab 3: Ο Εγκέφαλος ROS2 (TF, Converter, RViz)
 gnome-terminal --tab --title="3. Brain (ROS2)" -- bash -c "
 echo 'Περιμένω τη Γέφυρα...';
-sleep 15;
+sleep 25;
 echo 'Καθαρισμός περιβάλλοντος και φόρτωση ROS2...';
 # unset PYTHONPATH;
 source /opt/ros/galactic/setup.bash;
