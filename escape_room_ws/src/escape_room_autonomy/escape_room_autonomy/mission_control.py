b4af348135c629bdb0548f5fdfda7a8baabc4be6
Zeroed <cocacc3m@gmail.com>
2026-06-28 21:54:13 +0300

    debug

--- escape_room_ws/src/escape_room_autonomy/escape_room_autonomy/mission_control.py
+++ escape_room_ws/src/escape_room_autonomy/escape_room_autonomy/mission_control.py
@@ -172,6 +172,15 @@ class ExploreMazeAction(py_trees.behaviour.Behaviour):
         return py_trees.common.Status.RUNNING
 
     def get_best_frontier(self):
+        unique_vals = np.unique(grid)
+        self.node.get_logger().info(f"DEBUG: Τιμές στο grid: {unique_vals}")
+
+        # Έλεγχος για το unknown (δοκίμασε και με 127 και με άλλες τιμές αν δεις στο log)
+        unknown_mask = np.uint8(grid == 127) * 255
+        self.node.get_logger().info(f"DEBUG: Πλήθος Unknown pixels: {np.sum(unknown_mask > 0)}")
+
+        free_map = np.uint8(grid == 0) * 255
+        self.node.get_logger().info(f"DEBUG: Πλήθος Free pixels: {np.sum(free_map > 0)}")
         # Η "καθαρή" λογική που φτιάξαμε πριν
         grid = self.node.blackboard.grid_map
         res = self.node.blackboard.map_info.resolution
